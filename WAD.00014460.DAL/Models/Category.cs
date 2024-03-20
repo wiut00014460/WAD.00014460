c30851dff58d3e6969d7af369a1009e6bd8af872
wiut00014460 <wiut00014460@gmail.com>
2024-03-21 00:06:31 +0500

    Commit, models added

---
+++ WAD.00014460.DAL/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace WAD._00014460.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<TaskItem> Tasks { get; set; }
+    }
+}
---
+++ WAD.00014460.DAL/Models/TaskItem.cs
@@ -0,0 +1,18 @@
+﻿namespace WAD._00014460.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
