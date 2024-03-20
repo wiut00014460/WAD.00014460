7a247a37782832f41f5d8f79febda5e71a347afb
wiut00014460 <wiut00014460@gmail.com>
2024-03-21 00:07:01 +0500

    Data access layer added with db context and migration

---
+++ WAD.00014460.DAL/Mapping/AutoMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using WAD._00014460.DTOs;
+using WAD._00014460.Models;
+using AutoMapper;
+
+namespace WAD._00014460.Mapping
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<TaskItem, TaskItemDto>().ReverseMap();
+
+        }
+    }
+}
